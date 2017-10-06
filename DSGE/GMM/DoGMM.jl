05218dff1c5c60ef1937734fd6503aca2265ee2f
mcreel <michael.creel@uab.es>
2017-10-06 13:28:23 +0200

    update

--- DSGE/GMM/DoGMM.jl
+++ DSGE/GMM/DoGMM.jl
@@ -7,9 +7,9 @@ dsgedata = readdlm("simdata.design")
 # estimate by simulated annealing
 lb = lb_param_ub[:,1]
 ub = lb_param_ub[:,3]
-results = zeros(1000,21)
-#Threads.@threads for i = 1:1000
-for i = 1:1000
+results = zeros(1000,30)
+Threads.@threads for i = 1:100
+#for i = 1:100
     data = dsgedata[i,:]
     data = reshape(data, 160, 5)
     # define GMM criterion
@@ -23,10 +23,13 @@ for i = 1:1000
     thetahat, objvalue, converged, details = samin(obj, thetastart, lb, ub; ns = 20, verbosity = 1, rt = 0.9)
     # gradient-based for st. errors.
     thetahat2, objvalue2, converged = fmincon(obj, thetahat, [],[], lb, ub)
-    results[i,:] = [thetahat; thetahat2; details[end,1]; objvalue; objvalue2]
-    if i > 1
-        dstats(results[1:i,:])
-    end    
+    D = (Calculus.jacobian(m, vec(thetahat2), :central))
+    V = inv(D*weight*D')/n
+    se = sqrt.(diag(V))
+    results[i,:] = [thetahat; thetahat2; se; details[end,1]; objvalue; objvalue2]
+#    if i > 1
+#        dstats(results[1:i,:])
+#    end    
 end
 writedlm("GMM_results.out", results)
 
